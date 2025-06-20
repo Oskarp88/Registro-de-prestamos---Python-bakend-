e85482d9174bfd7d7c5791a8065a0eaff1c13036
oskarp88 <oskyburgos81@gmail.com>
2025-06-20 09:59:25 -0500

    corrigiendo url de mongo

--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,6 @@
 import motor.motor_asyncio
 import os
+
 if os.getenv("ENV") != "production":
     from dotenv import load_dotenv
     load_dotenv()
@@ -8,6 +9,11 @@ from utils.constants import Constants
 
 MONGO_URL = os.getenv(Constants.MONGO_URL)
 DATABASE_NAME = os.getenv(Constants.DATABASE_NAME)
+
+# ✅ Agrega logs para verificar que sí se lee correctamente
+print("🌐 ENV:", os.getenv("ENV"))
+print("🔐 MONGO_URL:", "Present" if MONGO_URL else "Missing")
+print("📂 DATABASE_NAME:", DATABASE_NAME)
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     MONGO_URL,
